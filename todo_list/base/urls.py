91581d62d67a7dfefe4ed91787da96492006970d
thealibrs <alibrs15@hotmail.comm>
2021-04-29 15:51:43 +0300

    asd

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
     path('task-create/', TaskCreate.as_view(), name = "task-create"),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name = "task-update"),
-    path('task-delete/<int:pk>/', TaskDelete.as_view(), name = "task-delete"),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name = "task-delete")
     
 ]
